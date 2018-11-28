9d5f58540f9caefce64b9b4cf6ed52178746b315
daniel <dutianming@quicloud.cn>
2018-11-28 11:35:02 +0800

    init

---
+++ predict.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 16:25:17 2018
+
+@author: shirhe-lyh
+"""
+
+import cv2
+import glob
+import numpy as np
+import os
+
+import data_provider
+import predictor
+import shutil
+
+
+def border_expand(image, value=255, output_height=320, output_width=320):
+    height, width = image.shape[:2]
+    max_size = max(height, width)
+    if len(image.shape) == 3:
+        expanded_image = np.zeros((max_size, max_size, 3)) + value
+    else:
+        expanded_image = np.zeros((max_size, max_size)) + value
+    if height > width:
+        pad_left = (height - width) // 2
+        expanded_image[:, pad_left:pad_left + width] = image
+    else:
+        pad_top = (width - height) // 2
+        expanded_image[pad_top:pad_top + height] = image
+    return expanded_image
+
+
+if __name__ == '__main__':
+    # images_dir = '/data2/raycloud/deep_image_matting/test_images'
+    images_dir = '/data1/shape_mask/datasets/images'
+    frozen_inference_graph_path = ('./frozon/' +
+                                   'frozen_inference_graph.pb')
+
+    matting_predictor = predictor.Predictor(frozen_inference_graph_path,
+                                            gpu_index='1')
+    for image_path in glob.glob(os.path.join(images_dir, '*.*')):
+        inputFilepath = image_path
+        filename_w_ext = os.path.basename(inputFilepath)
+        filename, file_extension = os.path.splitext(filename_w_ext)
+        shutil.copyfile(image_path, './pred_out/' + filename + file_extension)
+        image = cv2.imread(image_path)
+        # alpha = np.zeros(image.shape[:2])
+        # trimap = data_provider.trimap(alpha)
+
+        # image = border_expand(image)
+        # trimap = border_expand(trimap, value=0)
+
+        images = np.expand_dims(image, axis=0)
+        # trimaps = np.expand_dims(trimap, axis=0)
+
+        alpha_matte_p, alpha_matte_p_discrete = matting_predictor.predict(
+            images)
+
+        alpha_matte_p = np.squeeze(alpha_matte_p, axis=0)
+        alpha_matte_p_discrete = np.squeeze(alpha_matte_p_discrete, axis=0)
+        alpha_matte_p =  alpha_matte_p
+        alpha_matte_p_discrete =  alpha_matte_p_discrete
+
+        image_path_prefix = './pred_out/' + filename
+        output_path = image_path_prefix + '_alpha.jpg'
+        output_path_refined = image_path_prefix + '_refined_alpha.jpg'
+        cv2.imwrite(output_path, alpha_matte_p.astype(np.uint8))
+        cv2.imwrite(output_path_refined, alpha_matte_p_discrete.astype(np.uint8))
---
+++ predictor.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 11:49:09 2018
+
+@author: shirhe-lyh
+"""
+
+import os
+import tensorflow as tf
+
+
+class Predictor(object):
+    """Classify images to predifined classes."""
+    
+    def __init__(self,
+                 frozen_inference_graph_path,
+                 gpu_index=None,
+                 gpu_memory_fraction=None):
+        """Constructor.
+        
+        Args:
+            frozen_inference_graph_path: Path to frozen inference graph.
+            gpu_index: The GPU index to be used. Default None.
+        """
+        self._gpu_index = gpu_index
+        self._gpu_memory_fraction = gpu_memory_fraction
+        
+        self._graph, self._sess = self._load_model(frozen_inference_graph_path)
+        self._images = self._graph.get_tensor_by_name('image_tensor:0')
+        self.alpha_matte_p = self._graph.get_tensor_by_name('alpha_matte_p:0')
+        self.alpha_matte_p_discrete = self._graph.get_tensor_by_name('alpha_matte_p_discrete:0')
+        
+    def _load_model(self, frozen_inference_graph_path):
+        """Load a (frozen) Tensorflow model into memory.
+        
+        Args:
+            frozen_inference_graph_path: Path to frozen inference graph.
+            
+        Returns:
+            graph: A tensorflow Graph object.
+            sess: A tensorflow Session object.
+        
+        Raises:
+            ValueError: If frozen_inference_graph_path does not exist.
+        """
+        if not tf.gfile.Exists(frozen_inference_graph_path):
+            raise ValueError('`frozen_inference_graph_path` does not exist.')
+            
+        # Specify which gpu to be used.
+        if self._gpu_index is not None:
+            if not isinstance(self._gpu_index, str):
+                self._gpu_index = str(self._gpu_index)
+            os.environ['CUDA_VISIBLE_DEVICES'] = self._gpu_index
+        if self._gpu_memory_fraction is None:
+            self._gpu_memory_fraction = 1.0
+            
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(frozen_inference_graph_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+            
+        config = tf.ConfigProto(allow_soft_placement = True) 
+        config.gpu_options.per_process_gpu_memory_fraction = (
+            self._gpu_memory_fraction)
+        sess = tf.Session(graph=graph, config=config)
+        return graph, sess
+        
+    def predict(self, images, trimaps=None):
+        """Predict prediction tensors from inputs tensor.
+        
+        Args:
+            preprocessed_inputs: A 4D float32 tensor with shape [batch_size, 
+                height, width, channels] representing a batch of images.
+            
+        Returns:
+            classes: A 1D integer tensor with shape [batch_size].
+        """
+        feed_dict = {self._images: images}
+        [alpha_matte_p, alpha_matte_p_discrete] = self._sess.run(
+            [self.alpha_matte_p, self.alpha_matte_p_discrete],
+            feed_dict=feed_dict)
+        return alpha_matte_p, alpha_matte_p_discrete
+        

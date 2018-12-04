f917877365f2e3566778fca357b9397de288bb61
daniel <dutianming@quicloud.cn>
2018-12-04 12:47:39 +0800

    dataset generator

---
+++ dataset/generate_tfrecord.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 09:02:10 2018
+
+@author: tianming
+"""
+
+"""Generate tfrecord file from images.
+
+Example Usage:
+---------------
+python3 train.py \
+    --images_dir: Path to images (directory).
+    --annotation_path: Path to annotatio's .txt file.
+    --output_path: Path to .record.
+    --resize_side_size: Resize images to fixed size.
+"""
+
+import cv2
+import io
+import PIL.Image
+import tensorflow as tf
+import numpy as np
+
+import data_provider
+
+flags = tf.app.flags
+
+flags.DEFINE_string('images_fg_dir', 
+                    './fg_images',
+                    'Path to images (directory).')
+flags.DEFINE_string('images_bg_dir', 
+                    './bg_images',
+                    'Path to images (directory).')
+flags.DEFINE_string('trimaps_dir', 
+                    './gt_trimaps',
+                    'Path to images (directory).')
+flags.DEFINE_string('alpha_dir', 
+                    './gt_alphas',
+                    'Path to images (directory).')
+flags.DEFINE_string('annotation_path', 
+                    './correspondence.txt',
+                    'Path to fg_bg_mapping`s .txt file.')
+flags.DEFINE_string('output_path', 
+                    './train.record',
+                    'Path to output tfrecord file.')
+flags.DEFINE_integer('resize_side_size', None, 'Resize images to fixed size.')
+
+FLAGS = flags.FLAGS
+
+
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def int64_list_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def bytes_list_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def float_list_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def create_tf_example(image_fg_path, image_bg_path, trimap_path,
+                      resize_size=None):
+    image_fg = cv2.imread(image_fg_path, -1)
+    if image_fg is None:
+        print(image_fg_path)
+        return None
+    #image_fg = cv2.imdecode(np.fromfile(image_fg_path, dtype=np.uint8), -1)
+    alpha = data_provider.alpha_matte(image_fg)
+    alpha_dis = data_provider.alpha_matte_discrete(image_fg)
+    trimap = cv2.imread(trimap_path, 0)
+    if trimap is None:
+        print(trimap_path)
+        return None
+    image_fg = image_fg[:, :, :3]
+    image_bg = cv2.imread(image_bg_path)
+    image_fg = cv2.cvtColor(image_fg, cv2.COLOR_BGR2RGB)
+    image_bg = cv2.cvtColor(image_bg, cv2.COLOR_BGR2RGB)
+
+    height, width, _ = image_fg.shape
+
+    # Resize
+    if resize_size is not None:
+        if width > height:
+            width = int(width * resize_size / height)
+            height = resize_size
+        else:
+            width = resize_size
+            height = int(height * resize_size / width)
+        image_fg = cv2.resize(image_fg, (width, height))
+        alpha = cv2.resize(alpha, (width, height))
+        alpha_dis = cv2.resize(alpha_dis, (width, height))
+        trimap = cv2.resize(trimap, (width, height))
+    else:
+        image_bg = cv2.resize(image_bg, (width, height))
+
+    # Encode
+    pil_image_fg = PIL.Image.fromarray(image_fg)
+    bytes_io = io.BytesIO()
+    pil_image_fg.save(bytes_io, format='JPEG')
+    encoded_fg = bytes_io.getvalue()
+    pil_image_bg = PIL.Image.fromarray(image_bg)
+    bytes_io = io.BytesIO()
+    pil_image_bg.save(bytes_io, format='JPEG')
+    encoded_bg = bytes_io.getvalue()
+    pil_trimap = PIL.Image.fromarray(trimap)
+    bytes_io = io.BytesIO()
+    pil_trimap.save(bytes_io, format='JPEG')
+    encoded_trimap = bytes_io.getvalue()
+    pil_alpha = PIL.Image.fromarray(alpha)
+    bytes_io = io.BytesIO()
+    pil_alpha.save(bytes_io, format='JPEG')
+    encoded_alpha = bytes_io.getvalue()
+    pil_alpha_dis = PIL.Image.fromarray(alpha_dis)
+    bytes_io = io.BytesIO()
+    pil_alpha_dis.save(bytes_io, format='JPEG')
+    encoded_alpha_dis = bytes_io.getvalue()
+    
+    tf_example = tf.train.Example(
+        features=tf.train.Features(feature={
+            'image_fg/encoded': bytes_feature(encoded_fg),
+            'image/format': bytes_feature('jpg'.encode()),
+            'image_bg/encoded': bytes_feature(encoded_bg),
+            'trimap/encoded': bytes_feature(encoded_trimap),
+            'alpha_matte/encoded': bytes_feature(encoded_alpha),
+            'alpha_dis/encoded' : bytes_feature(encoded_alpha_dis),
+            'image/height': int64_feature(height),
+            'image/width': int64_feature(width)}))
+    return tf_example
+
+
+def generate_tfrecord(image_paths, output_path, resize_size=None):
+    num_valid_tf_example = 0
+    writer = tf.python_io.TFRecordWriter(output_path)
+    for image_fg_path, image_bg_path, trimap_path in image_paths:
+        if not tf.gfile.GFile(image_fg_path):
+            print('%s does not exist.' % image_fg_path)
+            continue
+        if not tf.gfile.GFile(image_bg_path):
+            print('%s does not exist.' % image_bg_path)
+            continue
+        tf_example = create_tf_example(image_fg_path, image_bg_path,
+                                       trimap_path, resize_size)
+        if tf_example is None:
+            continue
+        writer.write(tf_example.SerializeToString())
+        num_valid_tf_example += 1
+
+        if num_valid_tf_example % 100 == 0:
+            print('Create %d TF_Example.' % num_valid_tf_example)
+
+    writer.close()
+    print('Total create TF_Example: %d' % num_valid_tf_example)
+    print('The number of skiped images: %d' % (len(image_paths) -
+                                               num_valid_tf_example))
+    
+    
+def main(_):
+    images_fg_dir = FLAGS.images_fg_dir
+    images_bg_dir = FLAGS.images_bg_dir
+    trimaps_dir = FLAGS.trimaps_dir
+    annotation_path = FLAGS.annotation_path
+    record_path = FLAGS.output_path
+    resize_size = FLAGS.resize_side_size
+
+    image_paths = data_provider.provide(annotation_path)
+
+    generate_tfrecord(image_paths, record_path, resize_size)
+    
+    
+if __name__ == '__main__':
+    tf.app.run()
